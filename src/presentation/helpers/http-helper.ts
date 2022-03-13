8bb62b11d66e463921dfbd82b22dc82756cde9c3
LucasMGutierrez <lucas.machado.gutierrez@gmail.com>
2022-03-12 23:36:41 -0300

    refactor: moce server error to a helper method

--- src/presentation/helpers/http-helper.ts
+++ src/presentation/helpers/http-helper.ts
@@ -1,3 +1,9 @@
+import { ServerError } from '../errors/server-error';
 import { httpResponse } from '../protocols/http';
 
 export const badRequest = (error: Error): httpResponse<Error> => ({ statusCode: 400, body: error });
+
+export const serverError = (): httpResponse<Error> => ({
+  statusCode: 500,
+  body: new ServerError(),
+});
