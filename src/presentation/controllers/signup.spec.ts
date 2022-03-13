392f23898963776715312e1e471dd3be75770907
LucasMGutierrez <lucas.machado.gutierrez@gmail.com>
2022-03-13 00:52:21 -0300

    feat: check if password and passwordConfirmation are equal

--- src/presentation/controllers/signup.spec.ts
+++ src/presentation/controllers/signup.spec.ts
@@ -148,4 +148,22 @@ describe('SignUp Controller', () => {
     expect(httpResponse.statusCode).toEqual(500);
     expect(httpResponse.body).toEqual(new ServerError());
   });
+
+  test('should return 400 if password confirmation fails', () => {
+    const { sut, emailValidatorStub } = makeSut();
+    jest.spyOn(emailValidatorStub, 'isValid').mockReturnValueOnce(false);
+    const httpRequest = {
+      body: {
+        name: 'any_name',
+        email: 'invalid_email@mail.com',
+        password: 'any_password',
+        passwordConfirmation: 'invalid_password',
+      },
+    };
+
+    const httpResponse = sut.handle(httpRequest);
+
+    expect(httpResponse.statusCode).toEqual(400);
+    expect(httpResponse.body).toEqual(new InvalidParamError('passwordConfirmation'));
+  });
 });
