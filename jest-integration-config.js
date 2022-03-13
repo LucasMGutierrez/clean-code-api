2af38597e52f8fb9cad93e11e7a807e19d3bd716
LucasMGutierrez <lucas.machado.gutierrez@gmail.com>
2022-03-13 15:10:10 -0300

    chore: jest config

---
+++ jest-integration-config.js
@@ -0,0 +1,6 @@
+// eslint-disable-next-line
+const config = require('./jest.config');
+
+config.testMatch = ['**/*.test.ts'];
+
+module.exports = config;
---
+++ jest-unit-config.js
@@ -0,0 +1,6 @@
+// eslint-disable-next-line
+const config = require('./jest.config');
+
+config.testMatch = ['**/*.spec.ts'];
+
+module.exports = config;
