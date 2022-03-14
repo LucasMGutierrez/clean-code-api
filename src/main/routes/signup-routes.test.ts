3098d0cd53833d0d2822e04605598bcba89c6cca
LucasMGutierrez <lucas.machado.gutierrez@gmail.com>
2022-03-14 20:58:41 -0300

    feat: ensure signup route returns an account on success

---
+++ src/main/routes/signup-routes.test.ts
@@ -0,0 +1,11 @@
+import request from 'supertest';
+import app from '../config/app';
+
+describe('SignUp Routes', () => {
+  test('should return an account on sucess', async () => {
+    await request(app)
+      .post('/api/signup')
+      .send({ name: 'User', email: 'user@mail.com', password: '123', passwordConfirmation: '123' })
+      .expect(200);
+  });
+});
