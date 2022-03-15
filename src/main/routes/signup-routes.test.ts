587880bb80b7a4828a08e7f90fc9efca7c14dd99
LucasMGutierrez <lucas.machado.gutierrez@gmail.com>
2022-03-14 21:09:20 -0300

    chore: connect to mongodb at signup integration tests

--- src/main/routes/signup-routes.test.ts
+++ src/main/routes/signup-routes.test.ts
@@ -1,7 +1,23 @@
 import request from 'supertest';
+import { getMongoHelper, MongoHelper } from '../../infra/db/mongodb/helpers/mongo-helper';
 import app from '../config/app';
 
 describe('SignUp Routes', () => {
+  let mongoHelper: MongoHelper;
+
+  beforeAll(async () => {
+    mongoHelper = await getMongoHelper();
+  });
+
+  beforeEach(async () => {
+    mongoHelper = await getMongoHelper();
+    const accountCollection = mongoHelper.getCollection('accounts');
+    await accountCollection.deleteMany({});
+  });
+
+  afterAll(async () => {
+    await mongoHelper.disconnect();
+  });
   test('should return an account on sucess', async () => {
     await request(app)
       .post('/api/signup')
