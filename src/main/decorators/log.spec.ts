3aba4bc71534ac16dc156b148c3e85f2c527ed96
LucasMGutierrez <lucas.machado.gutierrez@gmail.com>
2022-03-18 00:39:23 -0300

    feat: ensure LogControllerDecorator returns the correct values

--- src/main/decorators/log.spec.ts
+++ src/main/decorators/log.spec.ts
@@ -61,4 +61,25 @@ describe('LogController Decorator', () => {
     expect(handleSpy).toHaveBeenCalledTimes(1);
     expect(handleSpy).toHaveBeenCalledWith(httpRequest);
   });
+
+  test('should call controller handle', async () => {
+    const { sut } = makeSut();
+    const httpRequest = {
+      body: {
+        email: 'any_email@mail.com',
+        name: 'any_name',
+        password: 'any_password',
+        passwordConfirmation: 'any_password',
+      },
+    };
+
+    const httpResponse = await sut.handle(httpRequest);
+
+    expect(httpResponse).toEqual({
+      body: {
+        name: 'any_name',
+      },
+      statusCode: 200,
+    });
+  });
 });
